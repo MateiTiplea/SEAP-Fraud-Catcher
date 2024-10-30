2262613223dde28812b3697207590a4428e5f00b
denisa2904 <mariadenisa2904@gmail.com>
2024-10-30 22:11:05 +0200

    + refactor scraper

---
+++ scrape/request_strategy.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+import requests
+
+
+class RequestStrategy(ABC):
+    @abstractmethod
+    def make_request(self, url, headers, body=None):
+        pass
+
+
+class GetRequestStrategy(RequestStrategy):
+    def make_request(self, url, headers, body=None):
+        return requests.get(url, headers=headers)
+
+
+class PostRequestStrategy(RequestStrategy):
+    def make_request(self, url, headers, body=None):
+        return requests.post(url, headers=headers, data=body)
