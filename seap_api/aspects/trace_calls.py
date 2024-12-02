a07e63b2ad1e88378b2896cded6539067f9bdb4d
Bianca Bojescu <bianca.bojescu@gmail.com>
2024-12-02 21:56:22 +0200

    trace calls

--- seap_api/aspects/trace_calls.py
+++ seap_api/aspects/trace_calls.py
@@ -1,31 +1,43 @@
 import logging
+import inspect
 from functools import wraps
 
 logger = logging.getLogger(__name__)
 
 call_graph = {}
 
-
 def trace_calls(func):
-    """
-    Monitorizează și înregistrează dependențele funcționale.
-    """
-
     @wraps(func)
     def wrapper(*args, **kwargs):
+        stack = inspect.stack()
+        caller_function_name = "Unknown Function"
+        caller_module_name = "Unknown Module"
+
+        for frame_info in stack[2:]:
+            module = inspect.getmodule(frame_info.frame)
+            caller_function_name = frame_info.function
+            if module:
+                caller_module_name = module.__name__
+                if caller_function_name != func.__name__ or module.__name__ != __name__:
+                    break
+
+        logger.info(f"Function '{func.__name__}' in module '{__name__}' was called by "
+                    f"'{caller_function_name}' in module '{caller_module_name}'.")
+
         parent = trace_calls.current_function
         trace_calls.current_function = func.__name__
+
         if parent:
-            call_graph.setdefault(parent, []).append(func.__name__)
-            logging.info(f"{parent} -> {func.__name__}")  # Debug output
+            call_graph.setdefault(f"{caller_module_name}.{parent}", []).append(func.__name__)
+            logger.info(f"{caller_module_name}.{parent} -> {func.__name__}")
         else:
-            logging.info(f"{func.__name__} (entry point)")  # Entry point of the call graph
+            logger.info(f"{caller_module_name}.{func.__name__} (entry point)")
+
         try:
             return func(*args, **kwargs)
         finally:
             trace_calls.current_function = parent
 
     return wrapper
-
 
 trace_calls.current_function = None
